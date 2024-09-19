e264882028cd7a266917513a182288285c9a19fa
Daya Adianto <addianto@users.noreply.github.com>
2024-09-19 10:52:53 +0700

    Refactor favorite page to use cubit

--- lib/wordpair/view/favorites_page.dart
+++ lib/wordpair/view/favorites_page.dart
@@ -1,31 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:namer_app/app_state.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:namer_app/wordpair/cubit/wordpair_cubit.dart';
 
 class FavoritesPage extends StatelessWidget {
   const FavoritesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final appState = context.watch<MyAppState>();
+    return BlocBuilder<WordPairCubit, WordPairState>(
+      builder: (context, state) {
+        if (state.favorites.isEmpty) {
+          return const Center(child: Text('No favorites yet.'));
+        }
 
-    if (appState.favorites.isEmpty) {
-      return const Center(child: Text('No favorites yet.'));
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(24),
-          child: Text('You have ${appState.favorites.length} favorites:'),
-        ),
-        ...appState.favorites.map((e) => e.asString).map(
-              (e) => ListTile(
-                leading: const Icon(Icons.favorite),
-                title: Text(e),
-              ),
+        return ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(24),
+              child: Text('You have ${state.favorites.length} favorites:'),
             ),
-      ],
+            ...state.favorites.map((e) => e.asString).map(
+                  (e) => ListTile(
+                    leading: const Icon(Icons.favorite),
+                    title: Text(e),
+                  ),
+                ),
+          ],
+        );
+      },
     );
   }
 }
