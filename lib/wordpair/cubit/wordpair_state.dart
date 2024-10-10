090e940f44638b6965e882b1a44f8d84f363afc0
Daya Adianto <addianto@users.noreply.github.com>
2024-10-10 13:48:17 +0700

    Pull up props getter to parent class

--- lib/wordpair/cubit/wordpair_state.dart
+++ lib/wordpair/cubit/wordpair_state.dart
@@ -9,18 +9,16 @@ sealed class WordPairState extends Equatable {
 
   final WordPair current;
   final List<WordPair> favorites;
+
+  @override
+  List<Object?> get props => [current, favorites];
 }
 
 final class WordPairInitial extends WordPairState {
   WordPairInitial() : super(current: WordPair.random(), favorites: []);
 
-  @override
-  List<Object?> get props => [current, favorites];
 }
 
 final class WordPairUpdated extends WordPairState {
   const WordPairUpdated({required super.current, required super.favorites});
-
-  @override
-  List<Object?> get props => [current, favorites];
 }
